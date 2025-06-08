e39e7c1d88d11dc7eb5543512155185f731d2f46
Coder-KDV <KeaneDuaneIvan.Vaz@MyGeorgian.ca>
2025-06-08 03:10:54 -0400

    Frosty Flight and Roller Madness added

---
+++ Roller madness/Assets/Scripts/WinGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+	void OnTriggerEnter(Collider collision)                     // used for things like bullets, which are triggers.  
+	{
+		SceneManager.LoadScene(2);
+	}
+}
